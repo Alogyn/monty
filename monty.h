30653394bbadd0aa6ec92a6f74426dcc59d3554f
Mohamed Derfoufi <138708102+Alogyn@users.noreply.github.com>
2023-12-14 23:44:02 +0100

    Prototypes & structures

--- monty.h
+++ monty.h
@@ -11,22 +11,22 @@
 #include <stdarg.h>
 
 /* Extern variable*/
-extern StackNode_t *head;
+extern stacknode_t *head;
 
 /**
-  * struct StackNode - Structure representing a node in a doubly linked list for a stack (or queue)
+  * struct stacknode - Structure representing a node in a doubly linked list for a stack (or queue)
   *
   * @n: Integer value stored in the node
   * @prev: Points to the previous node in the stack (or queue)
   * @next: Points to the next node in the stack (or queue)
   */
 
-typedef struct StackNode
+typedef struct stacknode
 {
         int n;
-        struct StackNode *prev;
-        struct StackNode *next;
-} StackNode_t;
+        struct stacknode *prev;
+        struct stacknode *next;
+} stacknode_t;
 
 /**
   * struct Instruction - Represents an opcode along with its corresponding function
@@ -38,15 +38,15 @@ typedef struct StackNode
 typedef struct Instruction
 {
         char *opcode;
-        void (*f)(StackNode_t **stack, unsigned int line_number);
+        void (*f)(stacknode_t **stack, unsigned int line_number);
 } Instruction_t;
 
-typedef void (*op_function)(StackNode_t **, unsigned int);
+typedef void (*op_function)(stacknode_t **, unsigned int);
 
 /* Main Functions */
-StackNode_t *create_node(int n);
+stacknode_t *create_node(int n);
 void free_nodes(void);
-void add_to_queue(StackNode_t **, unsigned int);
+void add_to_queue(stacknode_t **, unsigned int);
 
 /* File Operations Functions */
 void open_read_file(char *);
@@ -61,26 +61,26 @@ void handle_more_error(int error_code, ...);
 void handle_str_error(int error_code, ...);
 
 /* String Operations Functions */
-void print_char(StackNode_t **, unsigned int);
-void print_str(StackNode_t **, unsigned int);
-void rotate_left(StackNode_t **, unsigned int);
-void rotate_right(StackNode_t **, unsigned int);
+void print_char(stacknode_t **, unsigned int);
+void print_str(stacknode_t **, unsigned int);
+void rotate_left(stacknode_t **, unsigned int);
+void rotate_right(stacknode_t **, unsigned int);
 
 /* Math Operations Functions 1 */
-void multiply_nodes(StackNode_t **, unsigned int);
-void mod_nodes(StackNode_t **, unsigned int);
-void add_nodes(StackNode_t **, unsigned int);
+void multiply_nodes(stacknode_t **, unsigned int);
+void mod_nodes(stacknode_t **, unsigned int);
+void add_nodes(stacknode_t **, unsigned int);
 
 /* Math Operations Functions 2 */
-void sub_nodes(StackNode_t **, unsigned int);
-void div_nodes(StackNode_t **, unsigned int);
-void swap_nodes(StackNode_t **, unsigned int);
+void sub_nodes(stacknode_t **, unsigned int);
+void div_nodes(stacknode_t **, unsigned int);
+void swap_nodes(stacknode_t **, unsigned int);
 
 /* Stack Operations Functions */
-void add_to_stack(StackNode_t **, unsigned int);
-void print_stack(StackNode_t **, unsigned int);
-void pop_top(StackNode_t **, unsigned int);
-void print_top(StackNode_t **, unsigned int);
-void nop(StackNode_t **, unsigned int);
+void add_to_stack(stacknode_t **, unsigned int);
+void print_stack(stacknode_t **, unsigned int);
+void pop_top(stacknode_t **, unsigned int);
+void print_top(stacknode_t **, unsigned int);
+void nop(stacknode_t **, unsigned int);
 
 #endif /* MONTY_H */
