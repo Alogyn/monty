fb51737900a1a2f2c9991cf8a355357f45549525
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-25 06:44:44 -0800

    Prototypes & structures

--- monty.h
+++ monty.h
@@ -2,6 +2,8 @@
 #define MONTY_H
 
 #define _GNU_SOURCE
+
+/* Standard libreries */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,14 +12,13 @@
 #include <stdarg.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
+  * struct stack_s - doubly linked list representation of a stack (or queue)
+  *
+  * @n: integer
+  * @prev: points to the previous element of the stack (or queue)
+  * @next: points to the next element of the stack (or queue)
+  */
+
 typedef struct stack_s
 {
         int n;
@@ -26,19 +27,19 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
+  * struct instruction_s - opcode and its function
+  *
+  * @opcode: the opcode
+  * @f: function to handle the opcode
+  */
+
 typedef struct instruction_s
 {
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* Extern variable */
 extern stack_t *head;
 typedef void (*op_func)(stack_t **, unsigned int);
 
